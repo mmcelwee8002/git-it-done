f411ee0f34a94c6ab3bfb1107122aee0188aaeb0
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-01-02 18:18:59 -0600

    user repos updated

---
+++ assets/js/homepage.js
@@ -0,0 +1,16 @@
+
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+      console.log("outside"); 
+  getUserRepos();
