a58c86636fcae7a8b7ba6b49f5763bab1bab51d4
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-01-06 19:09:21 -0600

    final changes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -38,7 +38,7 @@ var buttonClickHandler = function(event) {
 var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
+console.log(response)
   // make a request to the url
   fetch(apiUrl)
     .then(function (response) {
